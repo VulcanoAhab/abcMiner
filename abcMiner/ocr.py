378b403a58a96d5aa10a4eb5445c19d73543cd9b
VulcanoAhab <vulcano.ahab@gmail.com>
2018-06-11 16:34:13 -0300

    First commit

--- abcMiner/ocr.py
+++ abcMiner/ocr.py
@@ -9,12 +9,14 @@ class Reader:
     OCR engines
     """
     @classmethod
-    def tesseract(cls, grayImage, config=None):
+    def plainTesseract(cls, grayImage, config=None, sizeScale=0):
         """
         grayImage: openCV Image Obj
         """
         size=grayImage.shape[:2]
-        newSize=tuple([int(s*3) for s in size[::-1]])
+        if not sizeScale:
+            sizeScale=int(1200/size[-1])
+        newSize=tuple([int(s*sizeScale) for s in size[::-1]])
         toImage=cv2.resize(grayImage, newSize)
         toTextImage=Image.fromarray(toImage)
         return image_to_string(toTextImage,config=config)
