adff3afd9783e325418faf4678f5edd43ebdf20e
VulcanoAhab <vulcano.ahab@gmail.com>
2018-06-12 21:51:00 -0300

    Starting browser screenshot

--- abcMiner/ocr.py
+++ abcMiner/ocr.py
@@ -8,8 +8,9 @@ class Reader:
     Will implement different
     OCR engines
     """
+
     @classmethod
-    def plainTesseract(cls, grayImage, config=None, sizeScale=0):
+    def onewordTesseract(cls, grayImage, config=None, sizeScale=0):
         """
         grayImage: openCV Image Obj
         """
@@ -19,4 +20,13 @@ class Reader:
         newSize=tuple([int(s*sizeScale) for s in size[::-1]])
         toImage=cv2.resize(grayImage, newSize)
         toTextImage=Image.fromarray(toImage)
-        return image_to_string(toTextImage,config=config)
+        if not config:
+            config="-psm 5" #read char by column :: no line size issue
+        resultis=image_to_string(toTextImage,config=config)
+        return "".join(reversed(resultis.split("\n")))
+
+    @classmethod
+    def browserScreenTesseract(cls, grayImage, config=None, sizeScale=0):
+        """
+        """
+        pass
